df307a32266e86e8bd7cfa13f87eeba0c7232df6
Tandi M <tandi.maulan@gmail.com>
2026-01-13 13:29:51 +0700

    adjustment security config

--- src/main/java/com/order/management/configs/SecurityConfig.java
+++ src/main/java/com/order/management/configs/SecurityConfig.java
@@ -2,8 +2,12 @@ package com.order.management.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SecurityConfig {
@@ -11,5 +15,11 @@ public class SecurityConfig {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+		UserDetails user = User.withUsername("admin").password(encoder.encode("admin123")).roles("ADMIN").build();
+		return new InMemoryUserDetailsManager(user);
 	}
 }
